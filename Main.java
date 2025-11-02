effa7be9d4de275a248574cc7c9b372713c19573
Enos ivory <enosyvori18@gmail.com>
2025-11-02 15:37:07 -0300

    Update output messages for postal code search

--- Main.java
+++ Main.java
@@ -11,14 +11,14 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         Gson gson = new GsonBuilder().setPrettyPrinting().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("\n busca endereço pelo cep, digite o CEP:  ");
+        System.out.println(" Busca de informações postais pelo CEP (Código de Endereçamento Postal) , digite o CEP:  ")
         String cep = scanner.nextLine();
 
 
         Requisicoes searchFilm = new Requisicoes();
         String busca = searchFilm.request(cep);
 
-        System.out.println("Retorno do APi viaCEP: ");
+        System.out.println("Retorno da APi viaCEP: ");
         System.out.println(gson.toJson(gson.fromJson(busca, Object.class)));
 
     }
