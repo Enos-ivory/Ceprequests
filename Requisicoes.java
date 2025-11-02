38f311acdb2413cfb1f64c7560dbb9b7270a0c01
Enos ivory <enosyvori18@gmail.com>
2025-11-02 15:24:49 -0300

    Improve request method with error handling
    Added error handling for HTTP request failures and invalid CEP responses.

--- Requisicoes.java
+++ Requisicoes.java
@@ -1,5 +1,5 @@
-package desafios;
 
+package desafios;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -11,15 +11,40 @@ public class Requisicoes {
 
     public String request(String search) throws IOException, InterruptedException {
         String cep = search;
-        String url = "https://viacep.com.br/ws/"+ cep + "/json";
-
+        String url = "https://viacep.com.br/ws/" + cep + "/json";
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-      ;
-        return   response.body();
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() != 200) {
+                throw new IOException("Erro na requisição: codígo " + response.statusCode());
+            }
+            String body = response.body();
+            if (body.contains("\"erro\": true")) {
+                throw new IllegalArgumentException("CEP não encontrado! ");
+            }
+            return body;
+
+        }  catch (IllegalArgumentException e) {
+                System.out.println("⚠️ Erro: " + e.getMessage());
+            } catch (IOException e) {
+                System.out.println("❌ Falha ao conectar à API: " + e.getMessage());
+            } catch (InterruptedException e) {
+                System.out.println("⚠️ A requisição foi interrompida!");
+                Thread.currentThread().interrupt(); // boa prática
+            } catch (Exception e) {
+                System.out.println("❗ Erro inesperado: " + e.getMessage());
+            }
+
+            return null; // caso dê erro, retorna nulo
         }
 
 }
+
+
+
+
+
+
